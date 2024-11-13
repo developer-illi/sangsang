ab21629c1035ceb467dc448e0d7f8db64c1c1f5a
user name <guswls8859@naver.com>
2024-11-13 16:17:03 +0900

    disable

---
+++ static/js/disable.js
@@ -0,0 +1,9 @@
+// 드래그 방지
+document.addEventListener('dragstart', function(event) {
+    event.preventDefault();
+});
+
+// 우클릭 방지
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
