046e10f03427f6e0114337cfb5f0e1973724b4a7
user name <guswls8859@naver.com>
2024-11-20 09:02:35 +0900

    testset

--- static/js/main.js
+++ static/js/main.js
@@ -10,8 +10,8 @@ window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     const header_bg = document.querySelector(".header_main");
     if (window.scrollY > 0) {
-        header.style.backgroundColor = "rgba(0, 0, 0, 0.2)"; // 스크롤 시 검정색 배경, 투명도 0.8
-        header_bg.style.backgroundColor = "rgba(0, 0, 0, 0.2)"; // 스크롤 시 검정색 배경, 투명도 0.8
+        header.style.backgroundColor = "rgba(0, 0, 0, 0.6)"; // 스크롤 시 검정색 배경, 투명도 0.8
+        header_bg.style.backgroundColor = "rgba(0, 0, 0, 0.6)"; // 스크롤 시 검정색 배경, 투명도 0.8
     } else {
         header.style.backgroundColor = "rgba(0, 0, 0, 0)"; // 스크롤 전 완전 투명
         header_bg.style.backgroundColor = "rgba(0, 0, 0, 0)"; // 스크롤 전 완전 투명
